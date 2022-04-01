bb88c57cae415fe13b15536b3e9c265b61f21ccd
Lucian Vrabiescu <lucian.vrabiescu@zynksoftware.com>
2022-04-01 10:31:42 +0300

    app start

---
+++ AcademicInfoClient/AcademicInfoServer/Repositories/StudentRepository.cs
@@ -0,0 +1,26 @@
+﻿using AcademicInfoServer.Models;
+
+namespace AcademicInfoServer.Repositories
+{
+    public class StudentRepository
+    {
+        private readonly List<Student> _studentRepository = new List<Student>();
+
+        StudentRepository()
+        {
+            _studentRepository.Append(new Student(1, "CS", 2, 927));
+            _studentRepository.Append(new Student(2, "CS", 3, 937));
+            _studentRepository.Append(new Student(3, "CS", 1, 917));
+        }
+
+        public void Add(Student student)
+        {
+            _studentRepository.Append(student);
+        }
+
+        public Student[] GetStudents()
+        {
+            return _studentRepository.ToArray();
+        }
+    }
+}
