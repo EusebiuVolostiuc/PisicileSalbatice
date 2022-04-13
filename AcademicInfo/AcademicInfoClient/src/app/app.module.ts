4e29e993d9a1c5ea5bdd39d1569603376dc0ef65
VrajaEmilia <100709158+VrajaEmilia@users.noreply.github.com>
2022-04-13 19:19:46 +0300

    design login component

--- AcademicInfo/AcademicInfoClient/src/app/app.module.ts
+++ AcademicInfo/AcademicInfoClient/src/app/app.module.ts
@@ -1,17 +1,25 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+const routes: Routes = [
+  {path:'', component:LoginFormComponent}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginFormComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule
+    RouterModule.forRoot(routes), BrowserModule, HttpClientModule, BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [RouterModule]
 })
 export class AppModule { }
