27e845c3178f8b07251d24f136d56eba61058b93
VrajaEmilia <100709158+VrajaEmilia@users.noreply.github.com>
2022-04-13 20:36:29 +0300

    nvm i like this one better
    needed:
    ng add @angular/material,
    npm i -s @angular/flex-layout @angular/cdk,

--- AcademicInfo/AcademicInfoClient/src/app/app.module.ts
+++ AcademicInfo/AcademicInfoClient/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StudentProfileComponent } from './student-profile/student-profile.component';
+
 
 const routes: Routes = [
   {path:'', component:LoginFormComponent}
@@ -13,10 +21,13 @@ const routes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    LoginFormComponent
+    LoginFormComponent,
+    StudentProfileComponent
   ],
   imports: [
-    RouterModule.forRoot(routes), BrowserModule, HttpClientModule, BrowserAnimationsModule
+    RouterModule.forRoot(routes), BrowserModule, HttpClientModule, BrowserAnimationsModule,FlexLayoutModule,
+    MatFormFieldModule,MatInputModule,MatButtonModule,MatCardModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent],
