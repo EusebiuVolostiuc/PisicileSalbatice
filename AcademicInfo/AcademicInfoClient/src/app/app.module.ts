82e17da841d7e1c8a4cb38f431f6742696cd6c0c
Lucian Vrabiescu <lucian.vrabiescu@zynksoftware.com>
2022-04-13 13:40:19 +0300

    login component

--- AcademicInfo/AcademicInfoClient/src/app/app.module.ts
+++ AcademicInfo/AcademicInfoClient/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule, HttpClientModule
