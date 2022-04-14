d3401c559b1190368746abbeeef19cdf2040313f
VrajaEmilia <100709158+VrajaEmilia@users.noreply.github.com>
2022-04-14 16:11:36 +0300

    form group

--- AcademicInfo/AcademicInfoClient/src/app/staff-profile/manage-students/student-form/student-form.component.ts
+++ AcademicInfo/AcademicInfoClient/src/app/staff-profile/manage-students/student-form/student-form.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 @Component({
   selector: 'app-student-form',
   templateUrl: './student-form.component.html',
   styleUrls: ['./student-form.component.css']
 })
 export class StudentFormComponent implements OnInit {
+  public studentForm: FormGroup
+  
 
   constructor() { }
 
   ngOnInit(): void {
-  }
+    this.studentForm = new FormGroup({
+      Name : new FormControl('', [Validators.required]),
+      Department : new FormControl('', [Validators.required]),
+      Year : new FormControl('', [Validators.required]),
+      Group : new FormControl('', [Validators.required])
+  })
+}
+addStudent() {
+  alert('e ok!');
+}
 
 }
