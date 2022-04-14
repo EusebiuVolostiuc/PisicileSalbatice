0a420a8514afe06ac473737fe80134d86159ede5
Eusebiu Volostiuc <evolostiuc@yahoo.ro>
2022-04-14 09:12:54 +0300

    Squashed commit of the following:
    commit 68718eba8927fd333024acd2d91eea8c9f1e1d0d
    Author: 927_Octavian_Vărzaru <72062666+tavi255@users.noreply.github.com>
    Date:   Wed Apr 13 22:37:14 2022 +0300
    
        finished crud for students
    
    commit 98e825fb0670b85e9d729873fdc4953ea81aab03
    Author: 927_Octavian_Vărzaru <72062666+tavi255@users.noreply.github.com>
    Date:   Wed Apr 13 20:18:50 2022 +0300
    
        updated project
    
    commit 3013245dc7fbebd4c48889bb00daa5851dfc59d3
    Author: 927_Octavian_Vărzaru <72062666+tavi255@users.noreply.github.com>
    Date:   Wed Apr 13 16:30:40 2022 +0300
    
        added Controller for students

---
+++ AcademicInfo/AcademicInfoServer/Models/User.cs
@@ -0,0 +1,16 @@
+﻿namespace AcademicInfoServer.Models
+{
+    public class User
+    {
+        public int UserName { get; set; }
+        public string password { get; set; }
+
+        public string accountType { get; set; }
+
+        
+
+
+
+
+    }
+}
