b11a632e84ecdf6edbc528b0a5b9ed97d10c5489
VrajaEmilia <100709158+VrajaEmilia@users.noreply.github.com>
2022-05-03 15:01:46 +0300

    student info when logged in

--- AcademicInfo/AcademicInfoClient/src/app/student-profile/student-profile.component.ts
+++ AcademicInfo/AcademicInfoClient/src/app/student-profile/student-profile.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Router} from "@angular/router";
+import {parseJson} from "@angular/cli/utilities/json-file";
 
 @Component({
   selector: 'app-student-profile',
@@ -9,18 +11,45 @@ import {HttpClient} from "@angular/common/http";
 })
 export class StudentProfileComponent implements OnInit {
   public addStudentForm: FormGroup
+  name: String;
+  department: String;
+  year: String;
+  group: String;
 
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,private router:Router) { }
 
   ngOnInit(): void {
-    this.addStudentForm = new FormGroup({
+    var token = localStorage.getItem('token');
+
+    var tokenise = "Bearer " + token;
+
+
+    var headers = new HttpHeaders().set("Authorization", tokenise);
+
+    const httpOptions = {
+      headers: headers
+    };
+
+    this.http.get('https://localhost:4200/api/student',httpOptions)
+      .subscribe(response => {
+        var student = Object.values(response)[0];
+        console.log(student);
+        this.name= student["Name"];
+        this.department=student["department"];
+        this.year=student["year"];
+        this.group=student["groupp"];
+      })
+
+    /*  this.addStudentForm = new FormGroup({
       userId : new FormControl('', [Validators.required]),
       department : new FormControl('', [Validators.required]),
       name : new FormControl('', [Validators.required]),
       year : new FormControl('', [Validators.required]),
       group: new FormControl('', [Validators.required])
     })
+    */
+
   }
 
   addStudent() {
@@ -35,5 +64,10 @@ export class StudentProfileComponent implements OnInit {
       .subscribe(response => {
         console.log('post response ', response);
       })
+  }
+
+  logout() {
+    //nuj ce mai trb
+    this.router.navigateByUrl("");
   }
 }
