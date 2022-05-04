c85811f4a2c7f00f0cea3ddd3dce9ef9d7fecc27
917_Octavian_Vărzaru <72062666+tavi255@users.noreply.github.com>
2022-05-04 14:13:20 +0300

    added the get staff function

--- AcademicInfo/AcademicInfoServer/Controllers/StaffController.cs
+++ AcademicInfo/AcademicInfoServer/Controllers/StaffController.cs
@@ -511,6 +511,55 @@ namespace AcademicInfoServer.Controllers
             return new JsonResult("Deleted succesfully!");
         }
 
+        [HttpGet("get_Staff")]
+
+        public IActionResult get_Staff()
+        {
+
+            string userID = Authentication.AccountController.getUserIDFromRequest(HttpContext.Request);
+
+            if (userID == null)
+            {
+                return BadRequest("Invalid Token");
+            }
+
+            int id = Convert.ToInt32(userID);
+
+
+            string q = "select * from staff where userID=" + id;
+
+            DataTable dt = new DataTable();
+
+            SqlDataReader dr;
+
+            Console.WriteLine(q);
+
+
+            string myConn = _configuration.GetConnectionString("AcademicInfo");
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(myConn))
+                {
+                    conn.Open();
+
+                    using (SqlCommand cmd = new SqlCommand(q, conn))
+                    {
+                        dr = cmd.ExecuteReader();
+                        dt.Load(dr);
+
+                        return new JsonResult(dt);
+
+                    }
+                }
+            }
+
+            catch (Exception ex)
+            { return new JsonResult(ex.Message); }
+
+
+        }
+
 
 
 
