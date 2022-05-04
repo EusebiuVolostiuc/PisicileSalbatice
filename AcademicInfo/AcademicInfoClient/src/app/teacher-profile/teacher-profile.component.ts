e2df789b14014c7052502c90c2078bd40dec61a9
VrajaEmilia <100709158+VrajaEmilia@users.noreply.github.com>
2022-05-04 13:05:46 +0300

    logout teacher

--- AcademicInfo/AcademicInfoClient/src/app/teacher-profile/teacher-profile.component.ts
+++ AcademicInfo/AcademicInfoClient/src/app/teacher-profile/teacher-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Component({
   selector: 'app-teacher-profile',
@@ -8,10 +10,31 @@ import { Component, OnInit } from '@angular/core';
 export class TeacherProfileComponent implements OnInit {
   name: any;
 
-  constructor() { }
+  constructor(private router:Router,private http: HttpClient) {
 
-  ngOnInit(): void {
-    this.name=localStorage.getItem("account");
   }
 
+  ngOnInit(): void {
+    var token = localStorage.getItem('token');
+
+    var tokenise = "Bearer " + token;
+
+
+    var headers = new HttpHeaders().set("Authorization", tokenise);
+
+    const httpOptions = {
+      headers: headers
+    };
+    this.http.get('https://localhost:4200/api/teacher/get_Teacher',httpOptions)
+      .subscribe(response => {
+        var teacher = Object.values(response)[0];
+        console.log(teacher);
+        this.name=teacher["Name"]
+      })
+  }
+
+  logout() {
+    this.router.navigateByUrl("");
+    localStorage.setItem("token","");
+  }
 }
