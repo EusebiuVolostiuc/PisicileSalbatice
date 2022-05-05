a4742b0a6ebbb179d5733b55784e675586a1f651
917_Octavian_Vărzaru <72062666+tavi255@users.noreply.github.com>
2022-05-05 17:17:49 +0300

    added get grades for teacher

--- AcademicInfo/AcademicInfoServer/Controllers/TeacherController.cs
+++ AcademicInfo/AcademicInfoServer/Controllers/TeacherController.cs
@@ -21,6 +21,58 @@ namespace AcademicInfoServer.Controllers
             _configuration = config;
         }
 
+        [HttpGet("get_Grades")]
+
+        public IActionResult get_Grades()
+        {
+
+            string userID = Authentication.AccountController.getUserIDFromRequest(HttpContext.Request);
+
+            if (userID == null)
+            {
+                return BadRequest("Invalid Token");
+            }
+
+            int id = Convert.ToInt32(userID);
+
+
+            string q = "select * from Grades where teacherID=" + id;
+
+            DataTable dt = new DataTable();
+
+            SqlDataReader dr;
+
+            Console.WriteLine(q);
+
+
+            string myConn = _configuration.GetConnectionString("AcademicInfo");
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(myConn))
+                {
+                    conn.Open();
+
+                    using (SqlCommand cmd = new SqlCommand(q, conn))
+                    {
+                        dr = cmd.ExecuteReader();
+                        dt.Load(dr);
+
+                        return new JsonResult(dt);
+
+                    }
+                }
+            }
+
+            catch (Exception ex)
+            { return new JsonResult(ex.Message); }
+
+
+
+
+        }
+
+
         [HttpGet("get_Courses")]
         public IActionResult get_Courses()
         {
